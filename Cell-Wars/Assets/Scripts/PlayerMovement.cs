c90b057d1eb4ebb53c56f0cd37d93822a32ec810
Tom Dove <casulornstein@gmail.com>
2020-01-14 20:20:57 +0000

    Add player movement script, remove placeholder sphere.

---
+++ Cell-Wars/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float movementForce = 20f;   //Magnitude of the force of the Player
+    [SerializeField]
+    float maxSpeed = 3f;
+    Vector2 movementDirection;
+    Rigidbody2D rigidbody;      //Player Rigidbody Component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        // Get the inputs from the controller / keyboard.
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Apply force to the player
+        rigidbody.AddForce(movementDirection * movementForce);
+
+        // Cap the players velocity at the max speed.
+        if(rigidbody.velocity.magnitude > maxSpeed)
+         {
+            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+         }
+    }
+}
