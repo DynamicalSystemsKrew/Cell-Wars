4b5d99f28df0dd8246155ba3df30a664f51a74a6
jamespellis2 <53198493+jamespellis2@users.noreply.github.com>
2020-01-19 18:32:03 +0000

    Ally cloning

---
+++ Cell-Wars/Assets/Scripts/AllyCloning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyCloning : MonoBehaviour
+{
+    public GameObject Ally;
+
+    [SerializeField]
+    int numClones = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numClones; i++)
+        {
+            Quaternion angle = Quaternion.AngleAxis(i  * (360 / numClones), Vector3.forward);
+            Vector3 relativePos = angle * Vector3.up;
+            Instantiate(Ally, transform.position + relativePos, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
